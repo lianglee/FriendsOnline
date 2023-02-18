d66f3fbea000be43e3afdd461d8b663abb3b4c78
Arsalan Shah <manactric@gmail.com>
2023-02-18 23:56:01 +0500

    Update view.php

--- plugins/default/friendsonline/view.php
+++ plugins/default/friendsonline/view.php
@@ -1,33 +1,38 @@
-<div class="ossn-page-contents">
-	<strong><?php echo ossn_print('friendsonline');?></strong>
-    <?php
-		$user 		= ossn_loggedin_user();
-		$time  		= time();
-		$intervals 	= 10;
-		$users = $user->getFriends($user->guid, array(
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (openteknik.com).ossn
+ * @author    OSSN Core Team <info@openteknik.com>
+ * @copyright (C) OpenTeknik LLC
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+
+$user       = ossn_loggedin_user();
+$time 		= time();
+$intervals  = 10;
+$users = $user->getFriends($user->guid, array(
 				'wheres'     => "(u.last_activity > {$time} - {$intervals})",
-		));
-		if($users){
-			$list = array();
-			foreach($users as $item){
-				//ossn 6.6 bug
-				if($item->guid == ossn_loggedin_user()->guid){
-						continue;	
-				}
-				$list[] = $item;
-			}
-			$count = $user->getFriends($user->guid, array(
-				'wheres'     => "(u.last_activity > {$time} - {$intervals})",
-				'count' => true,
-			));
-			echo ossn_plugin_view('output/users', array(
-						'users' => $list,												  
-			));
-			echo ossn_view_pagination($count, 10, array(
-						'offset_name' => 'online_users_page',								
-			));
-		} else {
-			echo ossn_print('whoisonline:no');	
+));
+
+echo "<div class='text-center'>";	 
+if($users){
+	foreach($users as $user) {
+		//ossn 6.6 bug
+		if($user->guid == ossn_loggedin_user()->guid){
+				continue;	
 		}
-	?>	
-</div>
+	?>
+	 <a class="" href="<?php echo ossn_site_url() . 'u/' . $user->username; ?>">
+			<img class="user-img" src="<?php echo $user->iconURL()->small;?>">
+      </a>
+<?php
+	}
+	$all = ossn_site_url('friendsonline');
+	$vall = ossn_print('friendsonline:viewall');
+	echo "<a class='d-block' href='{$all}'>{$vall}</a>";
+} else {
+	echo ossn_print('whoisonline:no');		
+}
+echo "</div>";
